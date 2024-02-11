66fdfaedc1cc25624f58d8e3f168efa98eb36c4a
Pinkshepz <dechawatarty@gmail.com>
2024-02-11 18:05:32 +0700

    Beta 0.10.2

--- src/app/course/[courses]/[quizsets]/interface.tsx
+++ src/app/course/[courses]/[quizsets]/interface.tsx
@@ -411,24 +411,24 @@ export default function QuizInterface ({
                         </div>
 
                         {/* 02 - Question */}
-                        <div className='relative min-h-max lg:h-[40vh] w-full py-2 flex flex-row flex flex-col'>
+                        <div className='relative min-h-max sm:h-[40vh] w-full py-2 flex flex-col sm:flex-row'>
                             {/* Question Image */}
                             {/* Portion 1 */}
                             {activeSelectedQuestions[currentQuiz].QuestionImageUrl ?
-                                <img className='w-full h-[40vh] lg:w-[40%] lg:h-full lg:mr-4 object-cover rounded-2xl bg-white/95 dark:bg-black/80'
+                                <img className='w-full sm:w-[40%] sm:h-full sm:mr-4 object-cover rounded-2xl bg-white/95 dark:bg-black/80'
                                     src={activeSelectedQuestions[currentQuiz].QuestionImageUrl} alt="" /> : null}
                                 
                             {/* Portion 2 */}
-                            <div className='flex flex-col h-full w-full px-2 lg:px-0 justify-between'>
+                            <div className='flex flex-col h-full w-full px-2 sm:px-0 mt-4 sm:mt-0 justify-between'>
                                 {/* 2 Icons */}
-                                <div className='flex flex-wrap justify-center lg:justify-start lg:mb-4 lg:mt-0 gap-2'>
-                                    <div className='flex items-center w-max px-2 py-1 text-lg font-bold lg:text-lg rounded-xl bg-white/50 dark:bg-zinc-900/70 border border-gray-300 dark:border-neutral-700'>
+                                <div className='flex flex-wrap justify-center sm:justify-start sm:mb-4 sm:mt-0 gap-2'>
+                                    <div className='flex items-center w-max px-2 py-1 text-sm font-bold sm:text-sm rounded-xl bg-white/50 dark:bg-zinc-900/70 border border-gray-300 dark:border-neutral-700'>
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="ml-1 w-6 h-6 text-indigo-600 dark:text-indigo-500">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" /></svg>
                                         <span className='ml-2 mr-1'>
                                             {activeSelectedQuestions[currentQuiz].Level}</span>
                                     </div>
-                                    <div className='hidden lg:flex items-center w-max px-2 py-1 text-lg font-bold lg:text-lg rounded-xl bg-white/50 dark:bg-zinc-900/70 border border-gray-300 dark:border-neutral-700'>
+                                    <div className='hidden lg:flex items-center w-max px-2 py-1 text-sm font-bold sm:text-sm rounded-xl bg-white/50 dark:bg-zinc-900/70 border border-gray-300 dark:border-neutral-700'>
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="ml-1 w-6 h-6 text-indigo-600 dark:text-indigo-500">
                                         <path fillRule="evenodd" d="M1.5 7.125c0-1.036.84-1.875 1.875-1.875h6c1.036 0 1.875.84 1.875 1.875v3.75c0 1.036-.84 1.875-1.875 1.875h-6A1.875 1.875 0 0 1 1.5 10.875v-3.75Zm12 1.5c0-1.036.84-1.875 1.875-1.875h5.25c1.035 0 1.875.84 1.875 1.875v8.25c0 1.035-.84 1.875-1.875 1.875h-5.25a1.875 1.875 0 0 1-1.875-1.875v-8.25ZM3 16.125c0-1.036.84-1.875 1.875-1.875h5.25c1.036 0 1.875.84 1.875 1.875v2.25c0 1.035-.84 1.875-1.875 1.875h-5.25A1.875 1.875 0 0 1 3 18.375v-2.25Z" clipRule="evenodd" />
                                     </svg>
@@ -438,18 +438,18 @@ export default function QuizInterface ({
                                 </div>
                                 
                                 {/* Question Text */}
-                                <div className='pb-4 pt-4 lg:py-8 px-2 lg:p-4 lg:mt-0 h-full font-bold text-xl xl:text-2xl text-left text-center flex justify-center items-center rounded-xl lg:bg-white/50 lg:dark:bg-zinc-900/70 lg:border lg:border-gray-300 dark:lg:border-neutral-700'>
+                                <div className='pb-4 pt-4 sm:py-8 px-2 sm:p-4 sm:mt-0 h-full font-bold text-xl xl:text-2xl text-left text-center flex justify-center items-center rounded-xl sm:bg-white/50 sm:dark:bg-zinc-900/70 sm:border sm:border-gray-300 dark:sm:border-neutral-700'>
                                 {activeSelectedQuestions[currentQuiz].Question}</div>
                             </div>
                         </div>
 
                         {/* 03 - Choice */}
-                        <div className='relative min-h-max h-full lg:min-h-0 lg:h-full w-full flex py-2 flex-col lg:flex-row gap-4'>
+                        <div className='relative h-max lg:min-h-0 lg:h-full w-full flex py-2 flex-col lg:flex-row gap-4'>
                             {ChoiceObject}
                         </div>
 
                         {/* 04 - Bottom action bar */}
-                        <div className='fixed bottom-0 lg:relative h-20 w-full pr-8 lg:pr-0 pt-2 pb-4 flex flex-row'>
+                        <div className='relative h-20 w-full pt-2 pb-4 flex flex-row'>
                             <div className="flex flex-col items-center content-center text-center">
                                 <button onClick={() => handleReload()} 
                                     className="text-xl px-4 py-3 group rounded-xl border border-gray-300 dark:border-neutral-700 bg-white/70 dark:bg-black/40">
