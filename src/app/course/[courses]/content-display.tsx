ba572645d197b6c6e429688d5685be907c298756
Pinkshepz <dechawatarty@gmail.com>
2024-03-04 22:46:02 +0700

    1.1.1

--- src/app/course/[courses]/content-display.tsx
+++ src/app/course/[courses]/content-display.tsx
@@ -142,12 +142,15 @@ export default function CourseContentDisplay({
 
     // Map resources
     topicData.PracticeRef && resources.push(
-      <div className='chip-sm-pri px-2 sm:px-0' id='chip-sm' key={topicData.PracticeRef}>
+      <Link 
+        href={{ pathname: "./course/[courses]/[contents]" }}
+        as={`/course/${courseData["ID"]}/${topicData.PracticeRef}`}
+        className='chip-sm-pri px-2 sm:px-0' id='chip-sm' key={topicData.PracticeRef}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-3 h-3 mr-1 hidden sm:inline">
           <path strokeLinecap="round" strokeLinejoin="round" d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
         </svg>
-        <a href={topicData.PracticeRef} target='_blank'>Practice {topicData.PracticeRef}</a>
-      </div>
+        <span>Practice {topicData.PracticeRef}</span>
+      </Link>
     );
 
     for (let ref_num = 1; true; ref_num++) {
