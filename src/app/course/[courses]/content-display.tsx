28403f076cde0f562bb305a4f677d19e66c08e60
Pinkshepz <dechawatarty@gmail.com>
2024-03-06 12:33:50 +0700

    1.1.2

--- src/app/course/[courses]/content-display.tsx
+++ src/app/course/[courses]/content-display.tsx
@@ -146,10 +146,18 @@ export default function CourseContentDisplay({
         href={{ pathname: "./course/[courses]/[contents]" }}
         as={`/course/${courseData["ID"]}/${topicData.PracticeRef}`}
         className='chip-sm-pri px-2 sm:px-0' id='chip-sm' key={topicData.PracticeRef}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-3 h-3 mr-1 hidden sm:inline">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-3 h-3 mr-1.5 hidden sm:inline">
           <path strokeLinecap="round" strokeLinejoin="round" d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
         </svg>
-        <span>Practice {topicData.PracticeRef}</span>
+        <div className='flex flex-row'>
+          <span>Practice</span>
+          <span className='flex items-center justify-center mx-0.5'>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
+                <path fillRule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
+            </svg>
+          </span>
+          <span>{topicData.PracticeRef}</span>
+        </div>
       </Link>
     );
 
@@ -158,7 +166,7 @@ export default function CourseContentDisplay({
       if (((topicData["ResourceName" + ref_num] !== undefined) && (topicData["ResourceLink" + ref_num] !== undefined)) === true) {
         resources.push(
           <div className='chip-sm-ter px-2 sm:px-0' id='chip-sm' key={topicData["ResourceLink" + ref_num]}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-3 h-3 mr-1 hidden sm:inline">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-3 h-3 mr-1.5 hidden sm:inline">
               <path strokeLinecap="round" strokeLinejoin="round" d="M6 6.878V6a2.25 2.25 0 0 1 2.25-2.25h7.5A2.25 2.25 0 0 1 18 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 0 0 4.5 9v.878m13.5-3A2.25 2.25 0 0 1 19.5 9v.878m0 0a2.246 2.246 0 0 0-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0 1 21 12v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6c0-.98.626-1.813 1.5-2.122" />
             </svg>
             <a href={topicData["ResourceLink" + ref_num]} target='_blank'>{topicData["ResourceName" + ref_num]}</a>
@@ -238,16 +246,23 @@ export default function CourseContentDisplay({
               <div className='p-3'>
                 <h4 className='mb-2'>{star.Title}</h4>
                 <p className='mb-3'>{star.Description}</p>
-                <div className='flex flex-row items-center'>
-                  <span className='flex flex-row items-center' id='chip-md'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4 mr-1">
+                <div className='flex flex-wrap items-center gap-2'>
+
+                  <div className={((star.Category == 'SUPERSTAR') ? 'chip-sm-sec' : 'chip-sm-pri') + ' px-2 sm:px-0'} id='chip-sm'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-3 h-3 mr-1.5 inline">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
                     </svg>
-                    {star.Category}
-                  </span>
-                  <p id='chip-md'>
-                    {star.Ref}
-                  </p>
+                    <div className='flex flex-row'>
+                      <span>{star.Category}</span>
+                      <span className='flex items-center justify-center mx-0.5'>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
+                            <path fillRule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
+                        </svg>
+                      </span>
+                      <span>{star.Ref}</span>
+                    </div>                    
+                  </div>
+
                   <p className='ml-1'>
                     {star.Members} Questions
                   </p>
