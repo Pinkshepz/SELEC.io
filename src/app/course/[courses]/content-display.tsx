c6c89adde968dd0b5599ebcf14be93e149710be4
Pinkshepz <dechawatarty@gmail.com>
2024-03-29 18:13:12 +0700

    1.1.8c

--- src/app/course/[courses]/content-display.tsx
+++ src/app/course/[courses]/content-display.tsx
@@ -150,7 +150,7 @@ export default function CourseContentDisplay({
         href={{ pathname: "./course/[courses]/[contents]" }}
         as={`/course/${courseData["ID"]}/${topicData.PracticeRef}`}
         className='p-1 min-w-max h-fit' id='chip-sm' key={topicData.PracticeRef}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="color-pri w-[26px] h-[26px] hidden sm:inline ml-1">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="color-pri w-[26px] h-[26px] inline ml-1">
           <path strokeLinecap="round" strokeLinejoin="round" d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
         </svg>
         <div className='flex flex-col ml-2 mr-1'>
@@ -167,7 +167,7 @@ export default function CourseContentDisplay({
         resources.push(
           <a href={topicData["ResourceLink" + ref_num]} target='_blank' 
           className='p-1 min-w-max h-fit' id='chip-sm' key={ref_num + 'resource'}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="color-sec w-[28px] h-[28] hidden sm:inline ml-1">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="color-sec w-[28px] h-[28] inline ml-1">
               <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
             </svg>
             <div className='flex flex-col ml-2 mr-1'>
@@ -273,8 +273,8 @@ export default function CourseContentDisplay({
               as={`/course/${courseData["ID"]}/${star["Ref"]}`}
               className='-card-hover relative flex flex-col text-white' id='card-main' key={star.ID}>
               <div className='relative overflow-hidden'>
-                <img src={star.ImageLink} alt="" className='h-56 w-full object-cover' />
-                <div className='absolute top-0 h-56 w-full bg-neutral-900/65'></div>
+                <img src={star.ImageLink} alt="" className='h-64 w-full object-cover' />
+                <div className='absolute top-0 h-64 w-full bg-neutral-900/65'></div>
               </div>
               <div className='absolute bottom-0 w-full p-3'>
                 <p className='font-semibold'>Topic Overall Practice</p>
