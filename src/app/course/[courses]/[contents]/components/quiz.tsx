a59c69d191aad3a6c2c6eb0f66726be28275ee1c
Pinkshepz <dechawatarty@gmail.com>
2024-05-13 23:18:36 +0700

    1.3.2

--- src/app/course/[courses]/[contents]/components/quiz.tsx
+++ src/app/course/[courses]/[contents]/components/quiz.tsx
@@ -56,87 +56,85 @@ export default function QuizInterface ({
     // =========================================
 
     const handleChoiceInteract = (choice_index: number, mode: string) => {
-        switch (mode) {
-            case "Flashcard":
-
-                setQuestionArray((prev) => ([
-                    ...prev.slice(0, interfaceParams.currentQuestion),
-                    {
-                        ...prev[interfaceParams.currentQuestion],
-                        choices: {
-                            ...prev[interfaceParams.currentQuestion].choices,
-                            [choice_index]: {
-                                ...prev[interfaceParams.currentQuestion].choices[choice_index],
-                                graded: !questionArray[interfaceParams.currentQuestion].choices[choice_index].graded
-                            }
-                        },
-                        graded: true
+        if (mode == "Flashcard") {
+            setQuestionArray((prev) => ([
+                ...prev.slice(0, interfaceParams.currentQuestion),
+                {
+                    ...prev[interfaceParams.currentQuestion],
+                    choices: {
+                        ...prev[interfaceParams.currentQuestion].choices,
+                        [choice_index]: {
+                            ...prev[interfaceParams.currentQuestion].choices[choice_index],
+                            graded: !questionArray[interfaceParams.currentQuestion].choices[choice_index].graded
+                        }
                     },
-                    ...prev.slice(interfaceParams.currentQuestion + 1, interfaceParams.questionNumber)
-                ]));
-
-                break;
-
-            case "One Answer":
-                for (let index = 0; index < Object.keys(questionArray[interfaceParams.currentQuestion].choices).length; index++) {
-                    
-                    // De-select all questions
+                    graded: !questionArray[interfaceParams.currentQuestion].choices[choice_index].graded
+                },
+                ...prev.slice(interfaceParams.currentQuestion + 1, interfaceParams.questionNumber)
+            ]));
+        } else if (!questionArray[interfaceParams.currentQuestion].graded) {
+            switch (mode) {
+                case "One Answer":
+                    for (let index = 0; index < Object.keys(questionArray[interfaceParams.currentQuestion].choices).length; index++) {
+                        
+                        // De-select all questions
+                        setQuestionArray((prev) => ([
+                            ...prev.slice(0, interfaceParams.currentQuestion),
+                            {
+                                ...prev[interfaceParams.currentQuestion],
+                                choices: {
+                                    ...prev[interfaceParams.currentQuestion].choices,
+                                    [index]: {
+                                        ...prev[interfaceParams.currentQuestion].choices[index],
+                                        selected: false
+                                    }
+                                }
+                            },
+                            ...prev.slice(interfaceParams.currentQuestion + 1, interfaceParams.questionNumber)
+                        ]));
+    
+                    }
+    
+                    // Select selected question
                     setQuestionArray((prev) => ([
                         ...prev.slice(0, interfaceParams.currentQuestion),
                         {
                             ...prev[interfaceParams.currentQuestion],
                             choices: {
                                 ...prev[interfaceParams.currentQuestion].choices,
-                                [index]: {
-                                    ...prev[interfaceParams.currentQuestion].choices[index],
-                                    selected: false
+                                [choice_index]: {
+                                    ...prev[interfaceParams.currentQuestion].choices[choice_index],
+                                    selected: !prev[interfaceParams.currentQuestion].choices[choice_index].selected
                                 }
                             }
                         },
                         ...prev.slice(interfaceParams.currentQuestion + 1, interfaceParams.questionNumber)
                     ]));
-
-                }
-
-                // Select selected question
-                setQuestionArray((prev) => ([
-                    ...prev.slice(0, interfaceParams.currentQuestion),
-                    {
-                        ...prev[interfaceParams.currentQuestion],
-                        choices: {
-                            ...prev[interfaceParams.currentQuestion].choices,
-                            [choice_index]: {
-                                ...prev[interfaceParams.currentQuestion].choices[choice_index],
-                                selected: !prev[interfaceParams.currentQuestion].choices[choice_index].selected
+    
+                    break;
+    
+                case "Multiple Answer":
+    
+                    setQuestionArray((prev) => ([
+                        ...prev.slice(0, interfaceParams.currentQuestion),
+                        {
+                            ...prev[interfaceParams.currentQuestion],
+                            choices: {
+                                ...prev[interfaceParams.currentQuestion].choices,
+                                [choice_index]: {
+                                    ...prev[interfaceParams.currentQuestion].choices[choice_index],
+                                    selected: !prev[interfaceParams.currentQuestion].choices[choice_index].selected
+                                }
                             }
-                        }
-                    },
-                    ...prev.slice(interfaceParams.currentQuestion + 1, interfaceParams.questionNumber)
-                ]));
-
-                break;
-
-            case "Multiple Answer":
-
-                setQuestionArray((prev) => ([
-                    ...prev.slice(0, interfaceParams.currentQuestion),
-                    {
-                        ...prev[interfaceParams.currentQuestion],
-                        choices: {
-                            ...prev[interfaceParams.currentQuestion].choices,
-                            [choice_index]: {
-                                ...prev[interfaceParams.currentQuestion].choices[choice_index],
-                                selected: !prev[interfaceParams.currentQuestion].choices[choice_index].selected
-                            }
-                        }
-                    },
-                    ...prev.slice(interfaceParams.currentQuestion + 1, interfaceParams.questionNumber)
-                ]));
-
-                break;
-        
-            default:
-                break;
+                        },
+                        ...prev.slice(interfaceParams.currentQuestion + 1, interfaceParams.questionNumber)
+                    ]));
+    
+                    break;
+            
+                default:
+                    break;
+            }
         }
     }
 
@@ -200,28 +198,57 @@ export default function QuizInterface ({
                         id={(_choice["selected"] 
                             ? questionArray[interfaceParams.currentQuestion].Mode == 'Flashcard'
                                 ? 'card-quiz-ter'
-                                : questionArray[interfaceParams.currentQuestion].Mode == 'MCQ' 
-                                ? 'card-quiz-pri'
-                                : 'card-quiz-sec'
+                                : questionArray[interfaceParams.currentQuestion].Mode == 'One Answer' 
+                                    ? 'card-quiz-pri'
+                                    : 'card-quiz-sec'
                             : 'card-quiz')}
                         className='relative p-1 w-full h-full font-bold text-lg sm:text-xl flex flex-col text-center items-center justify-center rounded-xl'>
                         
-                        <div className={"flex flex-wrap items-center justify-center px-2 gap-2 text-center " + (_choice["choice"] ? "py-1" : "")}>
+
+                        {/* Front text */}
+
+                        <div id='choice-front-text' className={"flex flex-wrap items-center justify-center px-2 gap-2 text-center " + (_choice["choice"] ? "py-1" : "")}>
                             {arrayToChips(_choice["choice"])}
                         </div>
+
+
+                        {/* Choice image */}
+
                         {_choice["choiceImageUrl"] ? 
                             <div className='flex h-full max-h-[35vh] p-1'>
                                 <img src={_choice["choiceImageUrl"]} alt=""
                                     className='h-full max-h-[40vh] xl:max-h-full w-full rounded-lg object-cover' />
                             </div> : null
                         }
+
+
+                        {/* Answer report */}
+
+                        {questionArray[interfaceParams.currentQuestion].Mode == 'Flashcard'
+                                ? <span id='answer-type'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                                    </svg>
+                                </span>
+                                : questionArray[interfaceParams.currentQuestion].Mode == 'One Answer' 
+                                    ? <span id='answer-type'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 7.5A2.25 2.25 0 0 1 7.5 5.25h9a2.25 2.25 0 0 1 2.25 2.25v9a2.25 2.25 0 0 1-2.25 2.25h-9a2.25 2.25 0 0 1-2.25-2.25v-9Z" />
+                                        </svg>
+                                    </span>
+                                    : <span id='answer-type'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 8.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v8.25A2.25 2.25 0 0 0 6 16.5h2.25m8.25-8.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-7.5A2.25 2.25 0 0 1 8.25 18v-1.5m8.25-8.25h-6a2.25 2.25 0 0 0-2.25 2.25v6" />
+                                        </svg>
+                                    </span>
+                            }
                     </button>
                 </div>
             );
         } else {
             ChoiceObject.push(
                 <div id='card-quiz-container' className='_graded h-full w-full rounded-xl' key={index}>
-                    <div id={(questionArray[interfaceParams.currentQuestion].Mode == "Flashcard")
+                    <button id={(questionArray[interfaceParams.currentQuestion].Mode == "Flashcard")
                         // In case of flashcard
                         ? 'card-quiz-ter'
                         // Incase of MCQ
@@ -235,21 +262,67 @@ export default function QuizInterface ({
                                 ? 'card-quiz-red' // Answer is False but choosed -> INCORRECT
                                 : 'card-quiz') // Answer is False and unchoosed -> CORRECT
                         }
-                    className='relative p-1 w-full h-full font-bold text-lg sm:text-xl flex flex-col text-center items-center justify-center rounded-xl'>
-                        
-                        <div className={'flex flex-wrap items-center justify-center px-2 gap-2 text-center ' + (_choice["choice"] ? "py-1" : "")}>{
-                            arrayToChips(_choice["choice"])
-                        }</div>
-                        {_choice["backText"] && <div className={'flex flex-wrap items-center justify-center px-2 gap-2 text-center font-medium ' + (_choice["choice"] ? "py-1" : "")}>{
-                            arrayToChips(_choice["backText"])
-                        }</div>}
-                        {_choice["choiceImageUrl"] ? 
-                            <div className='flex h-full max-h-[35vh] p-1'>
-                                <img src={_choice["choiceImageUrl"]} alt=""
-                                    className='h-full max-h-[40vh] xl:max-h-full w-full rounded-lg object-cover' />
-                            </div> : null
-                        }
-                    </div>
+                        className={'w-full h-full text-lg sm:text-xl ' + ((questionArray[interfaceParams.currentQuestion].Mode != "Flashcard") && 'cursor-default')}
+                        onClick={() => handleChoiceInteract(index, questionArray[interfaceParams.currentQuestion].Mode)}
+                        >
+                            
+                            {/* Choice front text */}
+
+                            {(questionArray[interfaceParams.currentQuestion].Mode != "Flashcard")
+                                && <div id='choice-front-text' className={(_choice["choice"] ? "py-1" : "")}>
+                                {arrayToChips(_choice["choice"])}
+                            </div>}
+
+
+                            {/* Choice back text */}
+
+                            {_choice["backText"] 
+                                && <div id='choice-back-text' className={(_choice["choice"] ? "py-1" : "")}>
+                                    {arrayToChips(_choice["backText"])}
+                                </div>
+                            }
+
+
+                            {/* Choice image */}
+
+                            {_choice["choiceImageUrl"] 
+                                ? <div id='choice-image' className='flex h-full max-h-[35vh] p-1'>
+                                    <img src={_choice["choiceImageUrl"]} alt=""
+                                        className='h-full max-h-[40vh] xl:max-h-full w-full rounded-lg object-cover' />
+                                </div>
+                                : null
+                            }
+
+
+                            {/* Answer report */}
+
+                            {(questionArray[interfaceParams.currentQuestion].Mode == "Flashcard")
+                                // In case of flashcard
+                                ? null
+                                // Incase of MCQ
+                                : _choice["answer"]
+                                    // Answer is True
+                                    ? (_choice["selected"] 
+                                        ? <span id='answer-report'>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-6 h-6">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                                            </svg>
+                                        </span> // selected is True and choosed -> CORRECT
+                                        : <span id='answer-report'>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                                            </svg>
+                                        </span>) // Answer is True but unchoosed -> MISSED ANSWER
+                                    // Answer is False
+                                    : (_choice["selected"] 
+                                        ? <span id='answer-report'>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-6 h-6">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                                            </svg>
+                                        </span> // Answer is False but choosed -> INCORRECT
+                                        : null) // Answer is False and unchoosed -> OK
+                            }
+                    </button>
                 </div>
             );
         }
@@ -342,9 +415,17 @@ export default function QuizInterface ({
                         <div className='flex items-center w-max px-1 py-1 rounded-xl'>
                             <h5 className='ml-2 mr-1'>
                             {questionArray[interfaceParams.currentQuestion].Mode == 'Flashcard'
-                                ? <span id="chip-quiz-neu" className='h-16'>{questionArray[interfaceParams.currentQuestion].Mode}</span>
+                                ? <span id="chip-quiz-neu" className='h-16'>
+                                    <span id='answer-type'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                                    </svg>
+                                </span>
+                                    {questionArray[interfaceParams.currentQuestion].Mode}
+                                </span>
                                 : questionArray[interfaceParams.currentQuestion].Mode == 'One Answer' 
-                                    ? <span id="chip-quiz-pri">{questionArray[interfaceParams.currentQuestion].Mode}</span>
+                                    ? <span id="chip-quiz-pri">
+                                        {questionArray[interfaceParams.currentQuestion].Mode}</span>
                                     : <span id="chip-quiz-sec">{questionArray[interfaceParams.currentQuestion].Mode}</span>}
                             </h5>
                         </div>
@@ -378,7 +459,7 @@ export default function QuizInterface ({
                     </div>
 
                     {/* 03 - Choice */}
-                    <div className={'-scroll-none relative h-full lg:h-full w-full flex flex-col lg:grid gap-4 lg:overflow-y-scroll ' + (Object.keys(questionArray[interfaceParams.currentQuestion].choices).length == 1 ? 'lg:grid-cols-1' : 'lg:grid-cols-2')}>
+                    <div className={'-scroll-none relative h-full lg:h-full w-full flex flex-col lg:grid gap-4 ' + (Object.keys(questionArray[interfaceParams.currentQuestion].choices).length == 1 ? 'lg:grid-cols-1' : 'lg:grid-cols-2')}>
                         {ChoiceObject}
                     </div>
 
