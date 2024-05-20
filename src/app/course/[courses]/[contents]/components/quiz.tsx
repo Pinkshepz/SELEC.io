409036acb17098406bea88cc622950266e5461de
Pinkshepz <dechawatarty@gmail.com>
2024-05-20 21:52:56 +0700

    1.3.5

--- src/app/course/[courses]/[contents]/components/quiz.tsx
+++ src/app/course/[courses]/[contents]/components/quiz.tsx
@@ -331,10 +331,33 @@ export default function QuizInterface ({
     // ===== SECTION III: ASIDE FX ======
     // ==================================
 
-    let question_nav: React.ReactNode[] = [];
+    let current_topic: string = questionArray[0].Topic;
+    let question_nav: React.ReactNode[] = [
+        !interfaceParams.shuffleQuestion && <h5>{current_topic}</h5>
+    ];
+    let same_topic_choice_nav: React.ReactNode[] = [];
 
     for (let i = 0; i < interfaceParams.questionNumber; i++) {
-        question_nav.push(
+        // Check topic
+        if ((current_topic != questionArray[i].Topic) && (!interfaceParams.shuffleQuestion)) {
+            // Push current topic questions
+            question_nav.push(
+                <div className='pb-4 grid grid-cols-5 gap-2'>
+                    {same_topic_choice_nav}
+                </div>
+            );
+
+            // Set new topic
+            current_topic = questionArray[i].Topic;
+            question_nav.push(
+                <h5>{current_topic}</h5>
+            );
+            
+            // Reset
+            same_topic_choice_nav = [];
+        }
+
+        same_topic_choice_nav.push(
             <button
                 id={(interfaceParams.currentQuestion == i)
                     ? 'card-nav-accent'
@@ -354,6 +377,13 @@ export default function QuizInterface ({
         );
     }
 
+    // Push current topic questions, lastly
+    question_nav.push(
+        <div className='pb-4 grid grid-cols-5 gap-2'>
+            {same_topic_choice_nav}
+        </div>
+    );
+
     return(
         <div className='flex flex-col'>
             <div id='quiz-two-cols-fixed' key='interface'>
@@ -368,7 +398,7 @@ export default function QuizInterface ({
                     </section>
 
                     <section className='mx-4 h-max overflow-y-scroll -scroll-none' key='interface-aside-section-2'>
-                        <div className='pb-4 grid grid-cols-5 gap-2'>
+                        <div className='flex flex-col gap-4'>
                             {question_nav}
                         </div>
                     </section>
@@ -405,12 +435,6 @@ export default function QuizInterface ({
                                     </svg>
                                 </span>
                                 : null }
-                        </div>
-                        <div id='card-quiz' className='hidden xl:flex items-center w-max px-1 py-1 rounded-xl'>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="ml-1 w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="m21 7.5-2.25-1.313M21 7.5v2.25m0-2.25-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3 2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75 2.25-1.313M12 21.75V19.5m0 2.25-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25" /></svg>
-                            <h5 className='ml-2 mr-1'>
-                                {questionArray[interfaceParams.currentQuestion].ID}</h5>
                         </div>
                         <div className='flex items-center w-max px-1 py-1 rounded-xl'>
                             <h5 className='ml-2 mr-1'>
@@ -459,7 +483,11 @@ export default function QuizInterface ({
                     </div>
 
                     {/* 03 - Choice */}
-                    <div className={'-scroll-none relative h-full lg:h-full w-full flex flex-col lg:grid gap-4 ' + (Object.keys(questionArray[interfaceParams.currentQuestion].choices).length == 1 ? 'lg:grid-cols-1' : 'lg:grid-cols-2')}>
+                    <div className={'-scroll-none relative h-full lg:h-full w-full flex flex-col lg:grid gap-4 ' + (questionArray[interfaceParams.currentQuestion].Mode == 'Flashcard'
+                        ? 'lg:grid-cols-1'
+                        : Object.keys(questionArray[interfaceParams.currentQuestion].choices).length == 1
+                            ? 'lg:grid-cols-1'
+                            : 'lg:grid-cols-2')}>
                         {ChoiceObject}
                     </div>
 
